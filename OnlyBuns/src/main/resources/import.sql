986e7e2ce1732ab2228ae068cec8e2132e18f0fd
lnosovic <lnosovic46@gmail.com>
2025-07-07 21:15:52 +0200

    Dodata mapa i jos neke stvari izmenjene

--- OnlyBuns/src/main/resources/import.sql
+++ OnlyBuns/src/main/resources/import.sql
@@ -1,4 +1,5 @@
 insert into location(longitude,latitude,country,city) values(20.2,45.2,'Serbia','Novi Sad');
+insert into location(longitude,latitude,country,city) values(20.4,44.8,'Serbia','Belgrade');
 
 insert into role (id,name) values(1,'ROLE_ADMIN');
 insert into role (id,name) values(2,'ROLE_USER');
@@ -6,8 +7,8 @@ insert into role (id,name) values(2,'ROLE_USER');
 insert into users (username,password,name,surname,email,location_id,is_activated,role_id,last_password_reset_date) values('pera123','$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra','Pera','Peric','pera@gmail.com',1,true,2,'2017-10-01 21:58:58.508-07');
 insert into users (username,password,name,surname,email,location_id,is_activated,role_id,last_password_reset_date) values('ana123','$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra','Ana','Anic','ana@gmail.com',1,true,2,'2017-10-01 21:58:58.508-07');
 
-insert into post(user_id,description,image,location_id,time_of_publishing) values(2, 'Short description','image1',1,'2025-01-06 21:30:00');
-insert into post(user_id,description,image,location_id,time_of_publishing) values(1, 'Short description','image1',1,'2017-10-01 20:30:00');
+insert into post(user_id,description,image,location_id,time_of_publishing) values(2, 'Short description','image1.jpg',1,'2025-01-06 21:30:00');
+insert into post(user_id,description,image,location_id,time_of_publishing) values(1, 'Short description','image1.jpg',2,'2017-10-01 20:30:00');
 
 
 insert into comment(user_id,post_id,text,created_at) values(1,1,'Good Work!!','2017-10-01 21:30:00');
